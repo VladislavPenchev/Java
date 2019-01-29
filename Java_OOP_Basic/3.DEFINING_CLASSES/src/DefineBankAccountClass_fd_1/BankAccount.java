e0e0911f2119db425d969c748c7d28ce5ae2cce7
VladislavPenchev <vgpenchev@gmail.com>
2019-01-29 14:45:10 +0200

    3.DefineBankAccountClass

---
+++ Java_OOP_Basic/3.DEFINING_CLASSES/src/DefineBankAccountClass_fd_1/BankAccount.java
@@ -0,0 +1,39 @@
+package DefineBankAccountClass_fd_1;
+
+public class BankAccount {
+    private int id;
+    private double balance;
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount){
+         if(this.getBalance() < amount){
+             throw new IllegalArgumentException("Insufficient balance");
+         }
+
+        this.balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Account ID%d, balance %.2f", this.id, this.balance);
+    }
+}
