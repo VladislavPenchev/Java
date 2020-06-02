2d86d127c4e278c3c9b11e32523bff937bd701a8
VladislavPenchev <34232950+VladislavPenchev@users.noreply.github.com>
2020-06-02 17:15:32 +0300

    add 25 workshop

---
+++ Java_MVC_Frameworks_Spring/25.WORKSHOP/Projects/Second_From_Video/src/test/java/org/softuni/deploymentdemo/DeploymentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softuni.deploymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
